a4ac3445f042732931f49909a93d8e3efe15f840
park kyung suk <boardguy1024@gmail.com>
2020-07-05 13:40:47 +0900

    notificationView追加

---
+++ InstagramSwiftUI/Notification/NotificationCell.swift
@@ -0,0 +1,31 @@
+//
+//  NotificationCell.swift
+//  InstagramSwiftUI
+//
+//  Created by park kyung suk on 2020/07/05.
+//  Copyright © 2020 park kyung suk. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+struct NotificationCell: View {
+    var body: some View {
+        VStack {
+            HStack (alignment: .top) {
+                Image("test").resizable().aspectRatio(contentMode: .fill).frame(width: 50, height: 50, alignment: .center).cornerRadius(5)
+                VStack (alignment: .leading) {
+                    Text("Park like your post").font(.callout)
+                    Text("One hour ago").font(.caption).foregroundColor(.gray)
+                }
+                Spacer()
+                }.padding()
+        }.background(Color.white).cornerRadius(10).shadow(radius: 8).padding()
+    }
+}
+
+struct NotificationCell_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationCell()
+    }
+}
--- InstagramSwiftUI/Notification/NotificationPage.swift
+++ InstagramSwiftUI/Notification/NotificationPage.swift
@@ -7,3 +7,27 @@
 //
 
 import Foundation
+import SwiftUI
+
+struct NotificationView: View {
+    
+    init() {
+        //ListのSeparatorColorを設定
+        UITableView.appearance().separatorColor = .clear
+    }
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(0 ..< 20, content: { i in
+                    NotificationCell().listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+                })
+            }.navigationBarTitle("Notifictaions", displayMode: .inline)
+        }
+    }
+}
+
+struct NotificationView_PreViews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
